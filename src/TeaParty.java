2633a541cb76cdcbd925d3de1ddb2c010273d2fb
student <student@ip-172-31-52-202.us-west-2.compute.internal>
2021-03-30 19:30:53 -0700

    TeaParty

---
+++ src/TeaParty.java
@@ -0,0 +1,28 @@
+public class TeaParty {
+	public String welcome(String name, boolean isWoman, boolean isKnighted) {
+
+		if (isWoman) {
+			if (isKnighted) {
+				return "Hello Lady " + name;
+
+			}
+
+			else {
+				return "Hello Ms. " + name;
+			}
+
+		}
+		
+		else {
+			if(isKnighted) {
+				return "Hello Sir " +name;
+			}
+			
+			else {
+				return "Hello Mr. " +name;
+			}
+			
+		}
+
+	}
+}
